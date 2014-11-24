6420938981e0a074da1cb709c4d2d6d66fffd012
EllaNancyFay <ellaschofield0@gmail.com>
2014-11-24 12:23:06 +0000

    Users can add tasks

--- Tickathon.js
+++ Tickathon.js
@@ -1,10 +1,28 @@
-Projects = new Mongo.Collection("tasks");
+Projects = new Mongo.Collection("projects");
 
 if (Meteor.isClient) {
 
   Template.body.helpers({
-   projects: function () {
-    return Projects.find({});
-   }
+
+    projects: function () {
+      return Projects.find({}, {sort: {createdAt: -1}});
+    }
   });
-}
+
+    Template.body.events({
+      "submit .new-project": function (event) {
+
+        var text = event.target.text.value;
+
+        Projects.insert({
+          text: text,
+          createdAt: new Date()
+        });
+
+        event.target.text.value = "";
+
+        return false;
+      }
+    });
+}  
+
